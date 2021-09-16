b7bdc424350040bdf8695f66e2bf4175e88bd262
Ben <benjamin.s.record@gmail.com>
2021-09-15 20:49:04 -0600

    deleted resources project

--- src/Resources/include/resources.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESOURCES_RESOURCES_H
-#define RESOURCES_RESOURCES_H
-
-#include <cstddef>
-
-void loadResource(const char *relativePath, void *buffer, size_t *outSize);
-
-#endif // RESOURCES_RESOURCES_H
--- src/Resources/src/resources.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/resources.h"
-
-void loadResource(const char *relativePath, void *buffer, size_t *outSize) {
-	// TODO
-}
