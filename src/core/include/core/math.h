31009f4ecacb1b96d4083781e49551081fee6132
Ben <benjamin.s.record@gmail.com>
2021-11-18 07:53:33 -0700

    basic collision checks

--- src/core/include/core/math.h
+++ src/core/include/core/math.h
@@ -4,6 +4,9 @@
 #include <cmath>
 #include <cstdint>
 
+#include <glm/vec2.hpp>
+#include <glm/vec4.hpp>
+
 namespace math {
 
     /**
@@ -13,7 +16,6 @@ namespace math {
      * @param current_upper bound of the value's current range
      * @param target_lower  bound of the value's target range
      * @param target_upper  bound of the value's target range
-     * @return
      */
     template<typename T>
     inline T map(T value, T current_lower, T current_upper, T target_lower, T target_upper) {
@@ -27,7 +29,6 @@ namespace math {
      * @param current_upper bound of the value's current range
      * @param target_lower  bound of the value's target range
      * @param target_upper  bound of the value's target range
-     * @return
      */
     inline float_t map(float_t value, float_t current_lower, float_t current_upper, float_t target_lower, float_t target_upper) {
         return map<float_t>(value, current_lower, current_upper, target_lower, target_upper);
@@ -40,7 +41,6 @@ namespace math {
      * @param current_upper bound of the value's current range
      * @param target_lower  bound of the value's target range
      * @param target_upper  bound of the value's target range
-     * @return
      */
     inline double_t map(double_t value, double_t current_lower, double_t current_upper, double_t target_lower, double_t target_upper) {
         return map<double_t>(value, current_lower, current_upper, target_lower, target_upper);
@@ -53,7 +53,6 @@ namespace math {
      * @param current_upper bound of the value's current range
      * @param target_lower  bound of the value's target range
      * @param target_upper  bound of the value's target range
-     * @return
      */
     inline int32_t map(int32_t value, int32_t current_lower, int32_t current_upper, int32_t target_lower, int32_t target_upper) {
         return map<int32_t>(value, current_lower, current_upper, target_lower, target_upper);
@@ -66,10 +65,22 @@ namespace math {
      * @param current_upper bound of the value's current range
      * @param target_lower  bound of the value's target range
      * @param target_upper  bound of the value's target range
-     * @return
      */
     inline uint32_t map(uint32_t value, uint32_t current_lower, uint32_t current_upper, uint32_t target_lower, uint32_t target_upper) {
         return map<uint32_t>(value, current_lower, current_upper, target_lower, target_upper);
+    }
+
+
+    /**
+     * Determines if position is within a region.
+     * @param pos       position in space to test whether it is within region
+     * @param region    region in space. x,y are obvious. z,w = width,height
+     */
+    inline bool is_within(const glm::vec2& pos, const glm::vec4& region) noexcept {
+        if (pos.x >= region.x && pos.x <= region.z) {
+            return pos.y >= region.y && pos.y <= region.w;
+        }
+        return false;
     }
 
 }
--- src/core/test/src/math_test.cpp
+++ src/core/test/src/math_test.cpp
@@ -25,3 +25,33 @@ TEST(Math, map_decimal) {
 
     ASSERT_EQ(expected, math::map(current, first_start, first_end, second_start, second_end));
 }
+
+TEST(Math, is_within) {
+    glm::vec4 region = glm::vec4(10, 20, 30, 40);
+
+    ASSERT_TRUE(math::is_within(glm::vec2(10, 20), region)) << "Top-left inclusive test expected pass.";
+    ASSERT_TRUE(math::is_within(glm::vec2(10, 40), region)) << "Bottom-left inclusive test expected pass.";
+
+    ASSERT_TRUE(math::is_within(glm::vec2(30, 20), region)) << "Top-right inclusive test expected pass.";
+    ASSERT_TRUE(math::is_within(glm::vec2(30, 40), region)) << "Bottom-right inclusive test expected pass.";
+
+
+    ASSERT_FALSE(math::is_within(glm::vec2(9, 20), region)) << "Top-left x-axis out of bounds test expected fail.";
+    ASSERT_FALSE(math::is_within(glm::vec2(9, 41), region)) << "Bottom-left x-axis out of bounds test expected fail.";
+
+    ASSERT_FALSE(math::is_within(glm::vec2(10, 19), region)) << "Top-left y-axis out of bounds test expected fail.";
+    ASSERT_FALSE(math::is_within(glm::vec2(10, 41), region)) << "Bottom-left y-axis out of bounds test expected fail.";
+
+    ASSERT_FALSE(math::is_within(glm::vec2(31, 20), region)) << "Top-right x-axis out of bounds test expected fail.";
+    ASSERT_FALSE(math::is_within(glm::vec2(31, 40), region)) << "Bottom-right x-axis out of bounds test expected fail.";
+
+    ASSERT_FALSE(math::is_within(glm::vec2(30, 19), region)) << "Top-right y-axis out of bounds test expected fail.";
+    ASSERT_FALSE(math::is_within(glm::vec2(30, 41), region)) << "Bottom-right y-axis out of bounds test expected fail.";
+
+    // test within region passes
+    for (size_t x = region.x; x < region.z; x++) {
+        for (size_t y = region.y; y < region.w; y++) {
+            ASSERT_TRUE(math::is_within(glm::vec2(x, y), region)) << "Inner region expected to pass.";
+        }
+    }
+}
