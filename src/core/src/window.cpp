abccd1b33c5c36f209f16a87ac783dcb3989e654
Ben <benjamin.s.record@gmail.com>
2021-11-21 22:08:33 -0700

    formatting

--- src/core/src/window.cpp
+++ src/core/src/window.cpp
@@ -2,8 +2,8 @@
 
 namespace window {
     float_t m_aspect_ratio = 1.0;
-    float_t m_width = 0.0;
-    float_t m_height = 0.0;
+    float_t m_width = 1.0;
+    float_t m_height = 1.0;
     display_profile_t m_display_profile;
 }
 
@@ -26,35 +26,56 @@ void window::set_size(float_t width, float_t height) {
 }
 
 size_t window::find_display_profile_width(display_profile_t display_profile) {
-    if (display_profile == DISPLAY_PROFILE_640_480) return 640;
-    if (display_profile == DISPLAY_PROFILE_1280_720) return 1280;
-    if (display_profile == DISPLAY_PROFILE_1920_1080) return 1920;
-    if (display_profile == DISPLAY_PROFILE_2560_1440) return 2560;
-    if (display_profile == DISPLAY_PROFILE_2048_1080) return 2048;
-    if (display_profile == DISPLAY_PROFILE_3840_2160) return 3840;
-    if (display_profile == DISPLAY_PROFILE_7680_4320) return 7680;
+    if (display_profile == DISPLAY_PROFILE_640_480)
+        return 640;
+    if (display_profile == DISPLAY_PROFILE_1280_720)
+        return 1280;
+    if (display_profile == DISPLAY_PROFILE_1920_1080)
+        return 1920;
+    if (display_profile == DISPLAY_PROFILE_2560_1440)
+        return 2560;
+    if (display_profile == DISPLAY_PROFILE_2048_1080)
+        return 2048;
+    if (display_profile == DISPLAY_PROFILE_3840_2160)
+        return 3840;
+    if (display_profile == DISPLAY_PROFILE_7680_4320)
+        return 7680;
     return 0;
 }
 
 size_t window::find_display_profile_height(display_profile_t display_profile) {
-    if (display_profile == DISPLAY_PROFILE_640_480) return 480;
-    if (display_profile == DISPLAY_PROFILE_1280_720) return 720;
-    if (display_profile == DISPLAY_PROFILE_1920_1080) return 1080;
-    if (display_profile == DISPLAY_PROFILE_2560_1440) return 1440;
-    if (display_profile == DISPLAY_PROFILE_2048_1080) return 1080;
-    if (display_profile == DISPLAY_PROFILE_3840_2160) return 2160;
-    if (display_profile == DISPLAY_PROFILE_7680_4320) return 4320;
+    if (display_profile == DISPLAY_PROFILE_640_480)
+        return 480;
+    if (display_profile == DISPLAY_PROFILE_1280_720)
+        return 720;
+    if (display_profile == DISPLAY_PROFILE_1920_1080)
+        return 1080;
+    if (display_profile == DISPLAY_PROFILE_2560_1440)
+        return 1440;
+    if (display_profile == DISPLAY_PROFILE_2048_1080)
+        return 1080;
+    if (display_profile == DISPLAY_PROFILE_3840_2160)
+        return 2160;
+    if (display_profile == DISPLAY_PROFILE_7680_4320)
+        return 4320;
     return 0;
 }
 
 display_profile_t window::find_display_profile(size_t width, size_t height) {
-    if (width == 640 && height == 480) return DISPLAY_PROFILE_640_480;
-    if (width == 1280 && height == 720) return DISPLAY_PROFILE_1280_720;
-    if (width == 1920 && height == 1080) return DISPLAY_PROFILE_1920_1080;
-    if (width == 2560 && height == 1440) return DISPLAY_PROFILE_2560_1440;
-    if (width == 2048 && height == 1080) return DISPLAY_PROFILE_2048_1080;
-    if (width == 3840 && height == 2160) return DISPLAY_PROFILE_3840_2160;
-    if (width == 7680 && height == 4320) return DISPLAY_PROFILE_7680_4320;
+    if (width == 640 && height == 480)
+        return DISPLAY_PROFILE_640_480;
+    if (width == 1280 && height == 720)
+        return DISPLAY_PROFILE_1280_720;
+    if (width == 1920 && height == 1080)
+        return DISPLAY_PROFILE_1920_1080;
+    if (width == 2560 && height == 1440)
+        return DISPLAY_PROFILE_2560_1440;
+    if (width == 2048 && height == 1080)
+        return DISPLAY_PROFILE_2048_1080;
+    if (width == 3840 && height == 2160)
+        return DISPLAY_PROFILE_3840_2160;
+    if (width == 7680 && height == 4320)
+        return DISPLAY_PROFILE_7680_4320;
     return 0;
 }
 
