b2b975c5625237e70225f8747a0d8e6326d01d2b
Ben <benjamin.s.record@gmail.com>
2021-11-25 13:55:52 -0700

    renamed to allocator

--- src/core/include/core/memory/element_buffer.h
+++ src/core/include/core/memory/element_buffer.h
@@ -1,14 +1,14 @@
 #ifndef CORE_MEMORY_ELEMENT_BUFFER_H
 #define CORE_MEMORY_ELEMENT_BUFFER_H
 
-#include "memory_zone.h"
+#include "memory_allocator.h"
 
 #include <cmath>
 #include <cstdint>
 
 struct ElementBuffer {
-    MemoryZone<float_t> vert_buffer;
-    MemoryZone<uint32_t> idx_buffer;
+    MemoryAllocator<float_t> vert_buffer;
+    MemoryAllocator<uint32_t> idx_buffer;
 
     ElementBuffer(size_t vert_buffer_size, size_t idx_buffer_size) : vert_buffer(nullptr, vert_buffer_size), idx_buffer(nullptr, idx_buffer_size) {}
 
